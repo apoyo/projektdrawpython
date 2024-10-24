797b83a2edc548346add883868cac332b4cb9859
apoyo14 <47917911+apoyo14@users.noreply.github.com>
2024-10-24 16:27:47 +0200

    with login

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
+from tkinter import messagebox
 from tkinter import *
 from PIL import Image, ImageTk
+import sqlite3
 root = tk.Tk()
 root.geometry("500x500")
 
@@ -19,19 +21,77 @@ root.title("Rysowanie Wykresów")
 #pack, place ,grid
 
 
-root.configure(bg='#333333')
-label = tk.Label(root,text = "Logowanie",font=('Arial',30),bg='#333333',fg='#FFFFFF' )
+root.configure(bg='#333333') 
+def login():
+    conn = sqlite3.connect('data.db')
+    username = myEntryLogin.get()
+    password = myEntryPassword.get()
+    table_create_query= '''CREATE TABLE IF NOT EXISTS logins(id INTEGER PRIMARY KEY,login varchar(25),password varchar(15),last_seen date ) '''
+    conn.execute(table_create_query)
+    cursorone = conn.cursor()
+    query_one = '''SELECT login from logins where login= ?'''
+    cursorone.execute(query_one,(username,))
+    #conn.commit()
+    result =  cursorone.fetchone()
+    print(result)
+    if result:
+        print("Przechodzi")
+        cursortwo = conn.cursor()
+        query_two = '''SELECT password from logins where login=?'''
+        cursortwo.execute(query_two,(username,))
+        result_final = cursortwo.fetchone()[0]
+      
+        print(result_final)
+        if result_final==password:
+           print("Jestesmy w miejscu docelowym")
+           messagebox.showinfo(title="info",message="Jestes Zalogowany")
+        else:
+           messagebox.showinfo(title="info",message="Haslo jest nieprawidłowe")
+     
+    else:
+        confirm = messagebox.askyesno(title="",message="Brak konta czy chcesz je założyć ?")
+        print(confirm)
+        if confirm:
+             insert_query ='''INSERT INTO logins (login,password) VALUES(?,?)'''
+             print("dodaje_raz")
+             insert_tuple = (myEntryLogin.get(),myEntryPassword.get())
+             cursor = conn.cursor()
+             cursor.execute(insert_query,insert_tuple)
+             conn.commit()
+             messagebox.showinfo(title="info",message="Jestes Zalogowany")
+        else:
+            messagebox.showinfo(title="Kursor",message= 'Nie załozono konta')
+             
+        
+
+    #password = ''''''
+    #if(myEntryLogin.get()== ,myEntryPassword.get()== )
+
+    conn.close()
+     
+   
+    #if myEntryLogin.get()==username and myEntryPassword.get()==password:
+     #   messagebox.showinfo(title="Login Success",message="Tou successfully logged in.")
+        #print("Successfully logged in")
+    #else:
+    #   messagebox.showinfo(title="Error",message="Invalid")
+    #  print("Invalid login")
+
+
+ 
+frame= tk.Frame(bg='#333333')
+label = tk.Label(frame,text = "Logowanie",font=('Arial',30),bg='#333333',fg='#FFFFFF' )
 label.grid(row=0,column=2,sticky="news",columnspan=2,pady=40)
-myLabelLogin = tk.Label(root,text = "Login",bg='#333333',fg='#FFFFFF',font=('Arial',16) )
-myEntryLogin = tk.Entry(root,font=('Arial',16) )
-myEntryPassword = tk.Entry (root,show="*",font=('Arial',16))
-myLabelPassword = tk.Label (root,text="Password",bg='#333333',fg='#FFFFFF',font=('Arial',16) )
-loginButton = tk.Button(root, text='Zaloguj' ,font =('Arial',18),bg="#FF3399",fg="#FFFFFF")
+myLabelLogin = tk.Label(frame,text = "Login",bg='#333333',fg='#FFFFFF',font=('Arial',16) )
+myEntryLogin = tk.Entry(frame,font=('Arial',16) )
+myEntryPassword = tk.Entry (frame,show="*",font=('Arial',16))
+myLabelPassword = tk.Label (frame,text="Password",bg='#333333',fg='#FFFFFF',font=('Arial',16) )
+loginButton = tk.Button(frame, text='Zaloguj' ,font =('Arial',18),bg="#FF3399",fg="#FFFFFF",command=login)
 myLabelLogin.grid(row=1,column=1)
 myLabelPassword.grid(row=2,column=1)
 myEntryLogin.grid(row=1,column=2,pady=20)
 myEntryPassword.grid(row=2,column=2,pady=20)
-loginButton.grid(row=3,column=2)
+loginButton.grid(row=3,column=2,columnspan=2,pady=30)
 
 #myEntryLogin.pack(padx=3,pady=3)
 #myEntryPassword.pack(padx=3,pady=3)
@@ -41,14 +101,14 @@ loginButton.grid(row=3,column=2)
 #loginButton.pack()
 
 
-inputtxt = tk.Text(root, height=3)
+inputtxt = tk.Text(frame, height=3)
 #inputtxt.pack(padx=10,pady=10)
 
-myEntry = tk.Entry(root)
+myEntry = tk.Entry(frame)
 #myEntry.pack()
 
-button = tk.Button(root, text="Click Me!", font = ('Arial', 18 ))
+button = tk.Button(frame, text="Click Me!", font = ('Arial', 18 ))
 
 #button.pack(padx=10,pady=10)
-
+frame.pack()
 root.mainloop()
